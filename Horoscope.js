7486107e544db98943032845da51e1e038b6a553
Adam <adam.lundquist1993@gmail.com>
2026-02-16 11:13:53 +0100

    First commit

---
+++ Horoscope.js
@@ -0,0 +1,9 @@
+/* Purpose of program:
+user inputs their sign, and the program generates a random horoscope message.
+To make the program truly random, the message that it outputs should be
+made up of at least three different pieces of data.
+
+Also
+    - Use Git version control
+    - Use command line
+*/
